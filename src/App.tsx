45ed4a89351a150877e3e9a5b5849059fb9c0535
Adrian Charles <adrian375charles@gmail.com>
2026-02-26 16:07:09 +0530

    Remove base path for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/carboneutrivo">
+      <BrowserRouter>  {/* Removed basename="/carboneutrivo" */}
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
